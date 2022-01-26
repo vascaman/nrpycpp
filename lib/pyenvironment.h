f647b157e90f784c77b6212575ae08da666ce745
Francesco Lamonica <f.lamonica@netresults.it>
2022-01-26 12:11:59 +0100

    removed unused includes

--- lib/pyenvironment.h
+++ lib/pyenvironment.h
@@ -1,9 +1,6 @@
 #ifndef PYENVIRONMENT_H
 #define PYENVIRONMENT_H
-#pragma push_macro("slots")
-#undef slots
-#include "Python.h"
-#pragma pop_macro("slots")
+
 #include "pyrunner.h"
 #include <QMap>
 
